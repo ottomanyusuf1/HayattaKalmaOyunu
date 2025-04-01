0d65ff41939dcb9eecff58261fd1672a3d7d0256
ottomanyusuf1 <kayaliyusuf9@gmail.com>
2025-04-01 17:15:53 +0300

    eşya alma
    yerden eşya alabiliyoruz (şuan sadece taş)

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -6,15 +6,29 @@ using UnityEngine.UI;
  
 public class SelectionManager : MonoBehaviour
 {
- 
+    
+    public static SelectionManager Instance {get; set;}
+    public bool onTarget;
     public GameObject interaction_Info_UI;
     Text interaction_text;
  
     private void Start()
     {
+        onTarget = false; 
         interaction_text = interaction_Info_UI.GetComponent<Text>();
     }
- 
+
+    private void Awake()
+    {
+        if(Instance !=null && Instance !=this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -22,20 +36,26 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
+
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
  
-            if (selectionTransform.GetComponent<InteractableObject>())
+            if (interactable && interactable.playerInRange)
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                onTarget = true;
+                
+                interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else 
             { 
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
             }
  
         }
         else
         {
+            onTarget = false;
             interaction_Info_UI.SetActive(false);
         }
     }
